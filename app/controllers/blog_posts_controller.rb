d6ae5f091c4739037358ab7f4fd5f2874465ce8c
JohnVonNeumann <louiswillcock@gmail.com>
2016-12-12 18:09:06 +1100

    edited the index view for blog posts, created a n ew and create controller which outputs through a url: object link in order to process form_for input in blog_posts/new

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,2 +1,15 @@
 class BlogPostsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    # we render, in plain text, the params entered to us from the new.html.erb form_for
+    # where we asked have a new blog_post object created on the input of information, the
+    # method then outputs to us via the index action, in simple formatted text
+    # notice the explicit use of the singular ':blog_post', not plural, indicating we are creating
+    # A blog_post, not "blog_posts" as a collection
+    render plain: params[:blog_post].inspect
+  end
+
 end
